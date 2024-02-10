4a40fe57595f6b0c9a5b4fd5296c8050b8f664e9
SirLinda <senzonkosi02@gmail.com>
2024-02-10 20:36:06 +0200

    Delete and update job

--- src/main/java/com/embarkx/jobapplication/job/JobController.java
+++ src/main/java/com/embarkx/jobapplication/job/JobController.java
@@ -1,26 +1,61 @@
 package com.embarkx.jobapplication.job;
 
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class JobController {
-    private List<Job> jobs = new ArrayList<>();
+    private final JobService jobService;
+
+    public JobController(final JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobs;
+    public ResponseEntity<List<Job>> findAll(){
+        return ResponseEntity.ok(jobService.findAll());
+    }
+    @GetMapping("jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
+
+        Job job = jobService.getJobById(id);
+        if(job!=null){
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
-        jobs.add(job);
-        return "Job created successfully";
+    public ResponseEntity<String> createJob(@RequestBody Job job){
+        jobService.createJob(job);
+        return  new ResponseEntity<>("Job added successfully", HttpStatus.OK);
     }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted = jobService.deleteJobById(id);
+
+        if (deleted){
+            return new ResponseEntity<>("Job deleted successfully", HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                            @RequestBody Job updateJob){
+        boolean updated = jobService.updateJob(id, updateJob);
+
+        if(updated){
+            return new ResponseEntity<>("Job updated successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
+
+    }
+
 }
---
+++ src/main/java/com/embarkx/jobapplication/job/JobServiceImpl.java
@@ -0,0 +1,64 @@
+package com.embarkx.jobapplication.job;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService{
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+
+    }
+
+    @Override
+    public Job getJobById(final Long id) {
+        for(Job job: jobs){
+            if (job.getId().equals(id)){
+                return job;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean deleteJobById(final Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()){
+            Job job = iterator.next();
+            if(job.getId().equals(id)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJob(final Long id, final Job updateJob) {
+        for (Job job:jobs){
+            if(job.getId().equals(id)){
+                job.setTitle(updateJob.getTitle());
+                job.setDescription(updateJob.getDescription());
+                job.setLocation(updateJob.getLocation());
+                job.setMinSalary(updateJob.getMinSalary());
+                job.setMaxSalary(updateJob.getMaxSalary());
+                return true;
+            }
+        }
+        return false;
+    }
+}
